dad9d71569e010e5fde7023ae4b0ca4fd9af033c
Vyacheslav <vyacheslav.job@yandex.ru>
2024-03-09 22:01:20 +0300

    migrate tripadvisor worker

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,1 @@
-export const tripadvisorAPIEndpoint = 'https://tripadvisor.millyadventurer8019.workers.dev/';
+export const tripadvisorAPIEndpoint = 'https://functions.yandexcloud.net/d4e0rf6ps1l15ud9f04k';
